0760c972837059f038103adceec1f3ad610a9a97
tayloregivens <31222544+tayloregivens@users.noreply.github.com>
2020-05-28 17:39:41 -0700

    Update header.js

--- src/header.js
+++ src/header.js
@@ -19,32 +19,154 @@ import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import * as am4maps from "@amcharts/amcharts4/maps";
 
+import am4themes_material from "@amcharts/amcharts4/themes/material";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import am4geodata_usaHigh from "@amcharts/amcharts4-geodata/usaHigh";
+import am4geodata_worldLow from "@amcharts/amcharts4-geodata/worldLow";
 
 // am4core.use;
 
 class Header extends Component {
   componentDidMount() {
     am4core.useTheme(am4themes_animated);
-    let map = am4core.create("chartdiv", am4maps.MapChart);
-    map.geodata = am4geodata_usaHigh;
-    map.projection = new am4maps.projections.AlbersUsa();
-    //series
-    let polygonSeries = new am4maps.MapPolygonSeries();
-    polygonSeries.useGeodata = true;
-    map.series.push(polygonSeries);
-    // ... chart code goes here ...
-    let polygonTemplate = polygonSeries.mapPolygons.template;
-    polygonTemplate.tooltipText = "{name}";
-    polygonTemplate.fill = am4core.color("#FF3600");
+    // Themes end
 
-    this.map = map;
+    // Create map instance
+    let chart = am4core.create("chartdiv", am4maps.MapChart);
+
+    let title = chart.titles.create();
+    title.text = "[bold font-size: 20]Universities with Most Donations[/]";
+    title.textAlign = "middle";
+
+    let mapData = [
+      {
+        id: "CA",
+        name: "Stanford",
+        value: 32358260,
+        color: chart.colors.getIndex(0),
+      },
+      {
+        id: "NY",
+        name: "New York University",
+        value: 3215988,
+        color: chart.colors.getIndex(1),
+      },
+      // {
+      //   id: "DZ",
+      //   name: "Algeria",
+      //   value: 35980193,
+      //   color: chart.colors.getIndex(2),
+      // },
+      {
+        id: "NY",
+        name: "NYU",
+        value: 3215988,
+        color: chart.colors.getIndex(1),
+      },
+      {
+        id: "AZ",
+        name: "Arizona State University",
+        value: 35980193,
+        color: chart.colors.getIndex(2),
+      },
+      {
+        id: "NV",
+        name: "University of Nevada",
+        value: 19618432,
+        color: chart.colors.getIndex(2),
+      },
+      {
+        id: "AR",
+        name: "University of Arkansas",
+        value: 40764561,
+        color: chart.colors.getIndex(3),
+      },
+      {
+        id: "FL",
+        name: "Florida",
+        value: 3100236,
+        color: chart.colors.getIndex(1),
+      },
+      { id: "TX", name: "Texas", value: 22605732, color: "#8aabb0" },
+      {
+        id: "VT",
+        name: "Vermont",
+        value: 8413429,
+        color: chart.colors.getIndex(1),
+      },
+      {
+        id: "OR",
+        name: "Oregon",
+        value: 9306023,
+        color: chart.colors.getIndex(1),
+      },
+      {
+        id: "WA",
+        name: "Washington",
+        value: 1323535,
+        color: chart.colors.getIndex(0),
+      },
+    ];
+
+    // Set map definition
+    chart.geodata = am4geodata_usaHigh;
+
+    // Set projection
+    chart.projection = new am4maps.projections.AlbersUsa();
+
+    // Create map polygon series
+    let polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+    // polygonSeries.exclude = ["AQ"];
+    polygonSeries.useGeodata = true;
+    polygonSeries.nonScalingStroke = true;
+    polygonSeries.strokeWidth = 0.5;
+    polygonSeries.calculateVisualCenter = true;
+
+    let imageSeries = chart.series.push(new am4maps.MapImageSeries());
+    imageSeries.data = mapData;
+    imageSeries.dataFields.value = "value";
+
+    let imageTemplate = imageSeries.mapImages.template;
+    imageTemplate.nonScaling = true;
+
+    let circle = imageTemplate.createChild(am4core.Circle);
+    circle.fillOpacity = 0.7;
+    circle.propertyFields.fill = "color";
+    circle.tooltipText = "{name}: [bold]${value}[/]";
+
+    imageSeries.heatRules.push({
+      target: circle,
+      property: "radius",
+      min: 4,
+      max: 30,
+      dataField: "value",
+    });
+    imageTemplate.adapter.add("latitude", function (latitude, target) {
+      let polygon = polygonSeries.getPolygonById(
+        "US-" + target.dataItem.dataContext.id
+      );
+      if (polygon) {
+        return polygon.visualLatitude;
+      }
+      return latitude;
+    });
+
+    imageTemplate.adapter.add("longitude", function (longitude, target) {
+      let polygon = polygonSeries.getPolygonById(
+        "US-" + target.dataItem.dataContext.id
+      );
+      if (polygon) {
+        return polygon.visualLongitude;
+      }
+      return longitude;
+    });
+
+    this.chart = chart;
   }
 
   componentWillUnmount() {
-    if (this.map) {
-      this.map.dispose();
+    if (this.chart) {
+      this.chart.dispose();
     }
   }
   render() {
