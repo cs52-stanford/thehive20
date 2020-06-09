65d1937334485989bfceecc78d784a2e4fa85e8c
Richard D Wesel <Rick@richardsmbp5024.home>
2020-06-08 21:55:10 -0700

    just names from the csv

--- src/cardTwo.js
+++ src/cardTwo.js
@@ -8,13 +8,8 @@ import rootRef from "./firebase.js";
 
 import moment from "moment";
 import { csv } from 'd3-request';
-import colleges from "./Colleges.csv";
+import colleges from "./CollegesForHive.csv";
 
-// var bigArray = [0, 0, 0, 0];
-// var names1;
-// var states1;
-// var latitudes1;
-// var longitudes1;
 
 
 const handleChange = (props, event) => {
@@ -27,24 +22,10 @@ const handleChange = (props, event) => {
       props.setEmail(event.target.value);
       break;
     case "entry3":
-      console.log("changing message");
       props.setMessage(event.target.value);
       break;
     case "entry4":
-      console.log("changing college name");
-      if(event.target.value == "Choose not to specify" || event.target.value == ""){
-        props.setSchool("None specified");
-        props.setState("No state");
-        props.setLatitude("No latitude");
-        props.setLongitude("No longitude");
-        console.log("No school");
-        break;
-      }
       props.setSchool(event.target.value);
-      props.setState(props.listOfStates[props.listOfSchools.findIndex(event.target.value)]);
-      props.setLatitude(props.listOfLats[props.listOfSchools.findIndex(event.target.value)]);
-      props.setLongitude(props.listOfLongs[props.listOfSchools.findIndex(event.target.value)]);
-      console.log("final answer", props.school, props.state, props.latitude, props.longitude);
       break;
   }
 }
@@ -68,7 +49,7 @@ function getMessage (p1) {
 }
 
 
-function getMapFromCSV(setListOfSchools, setListOfStates, setListOfLats, setListOfLongs){
+function getMapFromCSV(){
   
   return csv(colleges, function(err, data) {
     var bigArray1 = [0, 0, 0, 0];
@@ -77,20 +58,12 @@ function getMapFromCSV(setListOfSchools, setListOfStates, setListOfLats, setList
     } else{
       console.log(data);
       var dataset = data;
-      console.log("emily", dataset);
 
       var names = new Array(dataset.length).fill(0);
-      var states = new Array(dataset.length).fill(0);
-      var latitudes = new Array(dataset.length).fill(0);
-      var longitudes = new Array(dataset.length).fill(0);
       
       for (var i = 0; i < dataset.length; i++){
         names[i] = dataset[i].NAME;
-        states[i] = dataset[i].STATE;
-        latitudes[i] = dataset[i].LATITUDE;
-        longitudes[i] = dataset[i].LONGITUDE;
       }
-      console.log(names); //this works
     }
     console.log(names);
     var select = document.getElementById("productName"); 
@@ -101,11 +74,7 @@ function getMapFromCSV(setListOfSchools, setListOfStates, setListOfLats, setList
       el.value = opt;
       select.appendChild(el);
     }
-    console.log("names", names);//this workds
-    setListOfSchools(names);
-    setListOfStates(states);
-    setListOfLats(latitudes);
-    setListOfLongs(longitudes);
+    
    });
   }
 
@@ -132,33 +101,11 @@ const handleSubmit = (props) => {
 
 
 const DonationDetails = (props) =>  {
-  const [listOfSchools, setListOfSchools] = useState();
-  const [listOfStates, setListOfStates] = useState();
-  const [listOfLats, setListOfLats] = useState();
-  const [listOfLongs, setListOfLongs] = useState();
-  
   useEffect(() => {
-    for (var i = 0; i<10; i++){
-      getMapFromCSV(setListOfSchools, setListOfStates, setListOfLats, setListOfLongs);
-      console.log("schools", listOfSchools);
-      console.log("states", listOfStates);
-      console.log("lats", listOfLats);
-      console.log("longs", listOfLongs);
-    }
-
+    getMapFromCSV();
+    
   }, []);
   
-  //console.log("yikes", bigArray);
-  // names1 = bigArray[0];
-  // states1 = bigArray[1];
-  // latitudes1 = bigArray[2];
-  // longitudes1 = bigArray[3];
-  // console.log("names1", names1);
-  // console.log("states1", states1);
-  // console.log("longitudes1", longitudes1);
-  // console.log("latitudes1", latitudes1);
-  
-  // console.log("help!!!", names1);
   return <div id="donation-style">
     <textarea 
     id="entry1" 
@@ -188,10 +135,11 @@ const DonationDetails = (props) =>  {
     border-color: #CFCFCF; border-width: 0.1px;
     font-family: "courier"; font-size: 1rem;
     font-weight: 300; height: 40px; width:93.5%`} 
-      type="text" name="product" placeholder = " Select College" list="productName"/>
+      type="text" name="product" 
+      onChange={(event) => handleChange(props, event)}
+      placeholder = " Select College" list="productName"/>
       <datalist id="productName">
       <option value="Choose not to specify">Choose not to specify</option>
-
       </datalist>
 
     <div className={css`display: flex; flex-direction: row; justify-content: flex-end; width:100%`}>
@@ -212,10 +160,7 @@ const Second_Card = (props) => {
   const [email, setEmail] = useState();
   const [message, setMessage] = useState();
   const [map, setMap] = useState();
-  const [school, setSchool] = useState("no school");
-  const [state, setState] = useState();
-  const [longitude, setLongitude] = useState();
-  const [latitude, setLatitude] = useState();
+  const [school, setSchool] = useState("Choose not to specify");
   
   return <div 
     className={css`
@@ -246,8 +191,6 @@ const Second_Card = (props) => {
         {/* Hint: You'll be adding props to DonationDetails as you go!*/}
         <DonationDetails displayName={displayName} setDisplayName={setDisplayName} email={email}
         setEmail={setEmail} message={message} setMessage={setMessage} setIsFirstCard={props.setIsFirstCard}
-        state = {state} setState = {setState} latitude = {latitude} setLatitude = {setLatitude}
-        longitude = {longitude} setLongitude = {setLongitude}
         numTrees={props.numTrees} map = {map} setMap={setMap} school = {school} setSchool = {setSchool}/>
         <div
           className={css`
